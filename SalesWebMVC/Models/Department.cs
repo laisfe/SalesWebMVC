6626a8d1b153396196c46c1e01233541f7ffdee1
Lais Fernandes Fialho <laisffn@hotmail.com>
2020-06-22 15:03:58 -0300

    Other entities and second migration

--- SalesWebMVC/Models/Department.cs
+++ SalesWebMVC/Models/Department.cs
@@ -1,8 +1,34 @@
-﻿namespace SalesWebMVC.Models
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace SalesWebMVC.Models
 {
     public class Department
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public ICollection<Seller> Sellers { get; set; } = new List<Seller>();
+
+        public Department()
+        {
+        }
+
+        //Não inclui a lista de Seller no construtor por ser um Collection
+        public Department(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+
+        public void AddSeller(Seller seller)
+        {
+            Sellers.Add(seller);
+        }
+
+        public double TotalSales(DateTime initial, DateTime final)
+        {
+            return Sellers.Sum(s => s.TotalSales(initial, final));
+        }
     }
 }
