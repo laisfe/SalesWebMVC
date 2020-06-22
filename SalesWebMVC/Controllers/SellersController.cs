4d491b56f56a681f1e5ae387eb5bec42ea46b45e
Lais Fernandes Fialho <laisffn@hotmail.com>
2020-06-22 19:15:59 -0300

    SellersController

---
+++ SalesWebMVC/Controllers/SellersController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+//O controlador recebeu a chamada do localhost/Sellers
+namespace SalesWebMVC.Controllers
+{
+    public class SellersController : Controller
+    {
+        //Aciona a ação Index que retorna a View.
+        //A View irá até a pasta Views, procura-rá a pasta dentro 
+        //dela que tem o mesmo nome da controller atrás de um arquivo chamado Index
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
